769120d811da99fbf47d273cb8d7894d463bb301
Simon Martin <smartin@scottlogic.co.uk>
2016-05-27 15:58:47 +0100

    added attempt at exception handling

--- lib/pipeP.ts
+++ lib/pipeP.ts
@@ -4,19 +4,23 @@
 export function pipeP(head, ...promises): any {
     const context = this;
     return function () {
-        let result = head;
-        if(typeof head === 'function') {
-            result = head.call(context, ...arguments)
-        }
-        
-        return Promise.resolve(result).then((result) => {
-            if (promises.length == 0) {
-                return result
-            } else {
-                const [next, ...tail] = promises;
-
-                return pipeP(next, ...tail)(result)
+        try {
+            let result = head;
+            if (typeof head === 'function') {
+                result = head.call(context, ...arguments)
             }
-        })
+
+            return Promise.resolve(result).then((result) => {
+                if (promises.length == 0) {
+                    return result
+                } else {
+                    const [next, ...tail] = promises;
+
+                    return pipeP(next, ...tail)(result)
+                }
+            })
+        } catch (err) {
+            return Promise.reject(err.message);
+        }
     }
 }
