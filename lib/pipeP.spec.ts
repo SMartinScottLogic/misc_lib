bcb3a61bfb7725af49aab25278207ce2ff4d158d
Simon Martin <smartin@scottlogic.co.uk>
2016-05-27 16:43:57 +0100

    add exception test

--- lib/pipeP.spec.ts
+++ lib/pipeP.spec.ts
@@ -4,12 +4,15 @@ import {pipeP} from './pipeP';
 
 describe('pipeP()', function () {
     function sum2(a, b): Promise<number> {
-        console.log('sum2', a, b)
         return Promise.resolve(a + b);
     }
 
-    function increment(a): Promise<number> {
+    function increment(a): number {
         return a + 1;
+    }
+
+    function div_by_zero() {
+        throw new Error('div by zero')
     }
 
     it('chains promises', function (done) {
@@ -17,4 +20,16 @@ describe('pipeP()', function () {
             .then((result) => done(assert.equal(result, 5)))
             .catch(done);
     });
+
+    it('fails on exceptions', function (done) {
+        pipeP(sum2, increment, increment, 0, div_by_zero, console.log, increment, increment, increment, increment, increment)(1, 3)
+            .then((result) => {
+                console.log('at end')
+                done(new Error('at end'))
+            })
+            .catch((err) => {
+                assert(true)
+                done()
+            });
+    })
 });
