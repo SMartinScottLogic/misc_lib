c12da84c22c5b2c59bd656a7f9d8553932f6834e
Simon Martin <smartin@scottlogic.co.uk>
2016-06-03 15:30:22 +0100

    feeble attempt at jsdoc annotation

--- lib/reflectivePromise.ts
+++ lib/reflectivePromise.ts
@@ -1,6 +1,14 @@
 'use strict';
 
+/**  */
 export class ReflectivePromise {
+    /** 
+     * Create a function which waits for all promises to be completed, annotates the results with a custom key with a value describing the completion state
+     * @param {string} label_key - key to merge into each result
+     * @param {string} resolve_label - value to assign to the label_key, when the promise has successfully completed
+     * @param {string} reject_label - value to assign to the label_key, when the promise has not successfully completed (e.g. explicit error, throws)
+     * @return {function} 
+     */
     static all<T>(label_key: string = 'status', resolve_label: any = 'resolve', reject_label: any = 'reject'): (promises: Array<Promise<T>>) => Promise<Array<T>> {
         return function (promises: Array<Promise<T>>): Promise<Array<T>> {
             return Promise.all(<Array<any>>
