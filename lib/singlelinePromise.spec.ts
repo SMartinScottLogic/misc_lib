1374884ae2a32b4ced54ef4c734f3f0d6aaefaba
Simon Martin <smartin@scottlogic.co.uk>
2016-06-13 10:49:01 +0100

    added overwrite merge test re-ordered merge parameters, based on observed usage patterns (in tests)

--- lib/singlelinePromise.spec.ts
+++ lib/singlelinePromise.spec.ts
@@ -24,8 +24,18 @@ describe('merge', function() {
     it('merges results into transformed promise results', function(done) {
         Promise.resolve({})
         .then(merge((data)=>({test:true})))
-        .then(merge((data)=>({test2:false}), identity, (data)=>({data})))
+        .then(merge((data)=>({test2:false}), (data)=>({data})))
         .then((data)=>assert.deepEqual(data, {test:true,data:{test2:false}}))
+        .then((data)=>done())
+        .catch((err)=>done(err))
+    })
+
+    it('merges results into transformed promise results (with overwriting)', function(done) {
+        Promise.resolve({test2: '1234'})
+        .then(merge((data)=>({test: true})))
+        .then((result) => (assert.deepEqual(result, {test:true, test2: '1234'}),result))
+        .then(merge((data)=>({test:false}), ({test})=>({test2:test})))
+        .then((result)=>assert.deepEqual(result, {test:true,test2:false}))
         .then((data)=>done())
         .catch((err)=>done(err))
     })
--- lib/singlelinePromise.ts
+++ lib/singlelinePromise.ts
@@ -2,7 +2,7 @@
 
 export const identity = data => data;
 export const side_effect = (aside: (data: any) => any) => data => (aside(data), data)
-export const merge = (promise: (data: any) => any, extract: (data: any) => any = identity, transform: (data: any) => any = identity) =>
+export const merge = (promise: (data: any) => any, transform: (data: any) => any = identity, extract: (data: any) => any = identity) =>
     data => Promise.resolve(promise(extract(data)))
         .then(transform)
         .then(result => Object.assign({}, data, result))
